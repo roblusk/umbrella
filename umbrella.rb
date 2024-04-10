271efe2b6f5365a70696dcddadf688c1e0eb4142
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-10 03:01:06 +0000

    Successfully used pirate weather API to get temp

--- umbrella.rb
+++ umbrella.rb
@@ -3,17 +3,16 @@ pp "Where are you?"
 
 
 # Get and store the user’s location.
-# address = gets.chomp()
-address = "New York"
+address = gets.chomp()
 
 
 # Get the user’s latitude and longitude from the Google Maps API.
 address = address.strip # Remove excess spaces
-address = address.gsub(" ", "%20") # Replace spaces with "%20" to prepare the request for the HTTP request
+address_encoded = address.gsub(" ", "%20") # Replace spaces with "%20" to prepare the request for the HTTP request
 
 # Construct HTTP request
 gmaps_key = ENV.fetch("GMAPS_KEY")
-gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{gmaps_key}"
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address_encoded}&key=#{gmaps_key}"
 require "http"
 gmaps_raw_response = HTTP.get(gmaps_url)
 gmaps_parsed_response = JSON.parse(gmaps_raw_response)
@@ -24,4 +23,12 @@ location = geometry.fetch("location")
 lat = location.fetch("lat") #Float
 lng = location.fetch("lng") #Float
 
+# Get the weather at the user’s coordinates from the Pirate Weather API.
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{lat},#{lng}"
+pirate_weather_raw_response = HTTP.get(pirate_weather_url)
+pirate_weather_parsed_response = JSON.parse(pirate_weather_raw_response)
+currently = pirate_weather_parsed_response.fetch("currently")
+temperature = currently.fetch("temperature") #Float
+pp "The temperature at #{address} is #{temperature}F"
 
